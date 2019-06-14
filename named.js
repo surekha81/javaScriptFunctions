aca51e6e3eca26eb40b8da5131e197110dd4b9b1
surekha81 <51807711+surekha81@users.noreply.github.com>
2019-06-14 12:25:39 +1000

    Update named.js

--- named.js
+++ named.js
@@ -7,3 +7,7 @@ console.log(typeof hello);    // => 'function'
 function hello(name) {  
   return `Hello ${name}!`;
 }
+
+/*The function declaration function hello(name) {...} create a variable hello that is hoisted to the top of the current scope. 
+hello variable holds the function object and hello.name contains the function name: 'hello'.
+*/
