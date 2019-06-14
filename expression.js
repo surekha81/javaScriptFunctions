31bd0d8c5c323bce206ae318e9e82b4eddd06079
surekha81 <51807711+surekha81@users.noreply.github.com>
2019-06-14 13:21:06 +1000

    Update expression.js

--- expression.js
+++ expression.js
@@ -3,17 +3,19 @@ var isTruthy = function(value) {
   return !!value;
 };
 
-console.log(isTruthy())
+console.log(isTruthy());
 
 // Function expression: an argument for .filter()
 const doubledArray = function(arr) {
     var doubledValues = arr.filter(function(val) {return Number.isInteger(val) && val > 0 }).map(function(num) {return num * 2})
     return doubledValues;
-}
-console.log(doubledArray(numberedArray))
+};
+console.log(doubledArray(numberedArray));
 
 
 // Function expression : starts with "("
-(function messageFunction(message) {
+const message = (function messageFunction(message) {
   return message + ' World!';
 })('Hello');
+
+console.log(message);
